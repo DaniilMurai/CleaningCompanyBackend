4ce55473e49d2f46a083eb5debf38b6cb9bc159a
DaniilMurai <daniilmuray@gmail.com>
2025-05-30 20:27:40 +0300

    Fixing is_deleted column in Base

--- db/base.py
+++ db/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, Integer
+from sqlalchemy import Boolean, Column, Integer, false
 from sqlalchemy.orm import DeclarativeBase, declared_attr
 
 from utils.text import paschal_case_to_snake_case
@@ -6,7 +6,9 @@ from utils.text import paschal_case_to_snake_case
 
 class Base(DeclarativeBase):
     id = Column(Integer, primary_key=True, autoincrement=True)
-    is_deleted = Column(Boolean, default=False)
+    is_deleted = Column(
+        Boolean, server_default=false(), default=False, nullable=False
+    )
 
     @declared_attr
     def __tablename__(self):
