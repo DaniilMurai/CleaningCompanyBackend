24b4040691a81f5ca25a6dd4371714b7fb695561
DaniilMurai <daniilmuray@gmail.com>
2025-05-30 22:25:09 +0300

    Deleted file run_migrations.py

--- run_migrations.py
+++
@@ -1,7 +0,0 @@
-from alembic.config import CommandLine
-
-if __name__ == "__main__":
-    import subprocess
-
-    subprocess.run(["alembic", "revision", "--autogenerate", "-m", "auto"])
-    CommandLine().main(argv=["upgrade", "head"])
