8f32008421196228c2578f29ab707125ac86e6f1
DaniilMurai <daniilmuray@gmail.com>
2025-07-08 03:16:54 +0300

    fix

--- api/scheduler.py
+++ api/scheduler.py
@@ -5,14 +5,16 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from pytz import utc
 
 from db.crud.admin.daily_assignment import AdminDailyAssignmentCRUD
+from db.session import async_session_maker
 
 
 def start_daily_assignment_scheduler():
-    crud = AdminDailyAssignmentCRUD(...)
-    scheduler = BackgroundScheduler(timezone=utc)
-    scheduler.add_job(
-        lambda: asyncio.run(crud.mark_expired_assignments_as_not_completed()),
-        'cron',
-        hour=0, minute=1
-    )
-    scheduler.start()
+    with async_session_maker() as db:
+        crud = AdminDailyAssignmentCRUD(db=db)
+        scheduler = BackgroundScheduler(timezone=utc)
+        scheduler.add_job(
+            lambda: asyncio.run(crud.mark_expired_assignments_as_not_completed()),
+            'cron',
+            hour=0, minute=1
+        )
+        scheduler.start()
