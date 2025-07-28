abcad5b4ed9bdbb8c2e27360847b5c7916c952fd
@Semka_96 <max.skuybida@gmail.com>
2025-07-28 23:10:14 +0200

    Added logger setup to exports_worker

--- run_export_worker.py
+++ run_export_worker.py
@@ -1,9 +1,19 @@
 import asyncio
+import logging
 
+from config import settings
 from core.reports.export.worker import export_report_worker
+from loggers.setup import setup_logger
 
 
 async def main():
+    setup_logger(
+        name=settings.LOGGER_NAME,
+        logs_dir=settings.LOGS_DIR,
+        file_name="api.log",
+        level=logging.DEBUG,
+    )
+
     await asyncio.create_task(export_report_worker())
 
 
