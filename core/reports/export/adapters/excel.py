044e2eeade20e4b6573fef47f48a4f6bdac0f135
DaniilMurai <daniilmuray@gmail.com>
2025-08-10 20:52:43 +0300

    fix: xlsx formated columns len

--- core/reports/export/adapters/excel.py
+++ core/reports/export/adapters/excel.py
@@ -1,5 +1,6 @@
 from io import BytesIO
 
+import openpyxl.utils
 import pandas as pd
 
 from core.reports.export.adapters.base import ReportsAdapter
@@ -18,6 +19,16 @@ class ExcelAdapter(ReportsAdapter):
                 df[col] = df[col].dt.tz_localize(None)
 
             df.to_excel(writer, index=False, engine="openpyxl")
+            worksheet = writer.sheets["Sheet1"]
+
+            for i, col in enumerate(df.columns, 1):
+                max_length = max(
+                    df[col].astype(str).map(len).max(),
+                    len(col)
+                )
+                adjusted_width = max_length + 2
+                worksheet.column_dimensions[
+                    openpyxl.utils.get_column_letter(i)].width = adjusted_width
 
         output.seek(0)
         return output, "xlsx"
